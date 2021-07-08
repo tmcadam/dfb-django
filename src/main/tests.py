ed315cac5c7e280937c285cb3f7128659b5e46bd
Tom McAdam <mcadam1980@gmail.com>
2021-07-09 00:16:03 +1000

    Display environment in page title for dev and staging, and test

--- src/main/tests.py
+++ src/main/tests.py
@@ -1,5 +1,8 @@
 from django.test import TestCase
 from django.urls import reverse
+from django.conf import settings
+import os
+
 
 class MainViewTests(TestCase):
 
@@ -22,3 +25,16 @@ class MainViewTests(TestCase):
         self.assertContains(response, text='jquery', status_code=200)
         self.assertContains(response, text='bootstrap', status_code=200)
 
+
+    def test_page_title(self):
+        """
+        The title of the page (browser tab) contains the environment name, 
+        unless in production.
+        """
+        url = reverse('home')
+        settings.ENVIRONMENT = 'staging'
+        response = self.client.get(url)
+        self.assertContains(response, text='DFB - Staging', status_code=200)
+        settings.ENVIRONMENT = 'production'
+        self.assertContains(response, text='DFB', status_code=200)
+        self.assertNotContains(response, text='DFB - Production')
--- src/main/views.py
+++ src/main/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.conf import settings
 
 # Create your views here.
 def home(request):
-    return render(request, 'main/home.html')
+    context = {'env':settings.ENVIRONMENT}
+    return render(request, 'main/home.html', context)
