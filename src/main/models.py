b9e89b7140127be094d837ec8a4e010e8b657fa7
Tom McAdam <mcadam1980@gmail.com>
2021-07-10 14:34:35 +1000

    Created models for Biography and Country

--- src/main/models.py
+++ src/main/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Biography(models.Model):
+
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=50, db_index=True)
+    lifespan = models.CharField(max_length=50)
+    body = models.TextField()
+    authors = models.CharField(max_length=250)
+    revisions = models.TextField()
+    external_links = models.TextField()
+    references = models.TextField()
+    primary_country = models.ForeignKey('Country', on_delete=models.PROTECT, related_name='+')
+    secondary_country = models.ForeignKey('Country', on_delete=models.PROTECT, null=True, related_name='+')
+    south_georgia = models.BooleanField()
+    featured = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["title"]
+        verbose_name_plural = "Biographies"
+
+
+class Country(models.Model):
+
+    name = models.CharField(max_length=50, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- src/main/tests.py
+++ src/main/tests.py
@@ -1,7 +1,49 @@
+import os
 from django.test import TestCase
 from django.urls import reverse
 from django.conf import settings
-import os
+from django.db.utils import IntegrityError
+
+import factory
+
+from .models import *
+
+class CountryFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Country
+    name = factory.Faker('country')
+
+class BiographyFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Biography
+
+    title = factory.Faker('name')
+    slug = factory.Faker('slug')
+    body = factory.Faker('paragraphs', nb=6)
+    authors = factory.Faker('name')
+    revisions = factory.Faker('paragraph')
+    external_links = factory.Faker('paragraph')
+    references = factory.Faker('paragraph')
+    primary_country = factory.SubFactory(CountryFactory)
+    secondary_country= factory.SubFactory(CountryFactory)
+    south_georgia = factory.Faker('pybool')
+    featured= factory.Faker('pybool')
+
+class CountryModelTests(TestCase):
+
+    def test_create_country(self):
+        CountryFactory.create(name="Northern Ireland")
+        CountryFactory.create(name="Scotland")
+        self.assertEqual(Country.objects.all().count(), 2)
+
+    def test_create_country_fails_if_name_missing(self):
+        with self.assertRaises(IntegrityError):
+            CountryFactory.create(name=None)
+
+    def test_create_country_fails_if_not_unique(self):
+        CountryFactory.create(name="Northern Ireland")
+        with self.assertRaises(IntegrityError):
+            CountryFactory.create(name="Northern Ireland")
 
 
 class MainViewTests(TestCase):
