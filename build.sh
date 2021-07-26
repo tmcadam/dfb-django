ca34ad322e10e00e6e94e0fd515cbf756420488b
Tom McAdam <mcadam1980@gmail.com>
2021-07-26 23:33:07 +1000

    Updated scripts for new file locations

--- build.sh
+++ build.sh
@@ -3,32 +3,27 @@
 BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 mkdir -p ${BASE_DIR}/tmp
-mkdir -p ${BASE_DIR}/src/static/vendor
+mkdir -p ${BASE_DIR}/static/vendor
 
 # Install bootstrap to static folder
 wget https://github.com/twbs/bootstrap/archive/v4.1.3.zip -P ${BASE_DIR}/tmp
 unzip -d ${BASE_DIR}/tmp ${BASE_DIR}/tmp/v4.1.3.zip
-mkdir -p ${BASE_DIR}/src/static/vendor/bootstrap
-cp -r ${BASE_DIR}/tmp/bootstrap-4.1.3/scss/* ${BASE_DIR}/src/static/vendor/bootstrap
+mkdir -p ${BASE_DIR}/static/vendor/bootstrap
+cp -r ${BASE_DIR}/tmp/bootstrap-4.1.3/scss/* ${BASE_DIR}/static/vendor/bootstrap
 
 # Install fontawesome to static folder
 wget https://fontawesome.com/v4.7/assets/font-awesome-4.7.0.zip -P ${BASE_DIR}/tmp
 unzip -d ${BASE_DIR}/tmp ${BASE_DIR}/tmp/font-awesome-4.7.0.zip
-mkdir -p ${BASE_DIR}/src/static/vendor/fontawesome
-cp -r ${BASE_DIR}/tmp/font-awesome-4.7.0/scss/* ${BASE_DIR}/src/static/vendor/fontawesome
+mkdir -p ${BASE_DIR}/static/vendor/fontawesome
+cp -r ${BASE_DIR}/tmp/font-awesome-4.7.0/scss/* ${BASE_DIR}/static/vendor/fontawesome
 
 
 # Install Fonts
-mkdir -p ${BASE_DIR}/src/static/vendor/fonts
+mkdir -p ${BASE_DIR}/static/vendor/fonts
 wget https://www.1001fonts.com/download/open-sans.zip  -P ${BASE_DIR}/tmp
-unzip -o -d ${BASE_DIR}/src/static/vendor/fonts ${BASE_DIR}/tmp/open-sans.zip
+unzip -o -d ${BASE_DIR}/static/vendor/fonts ${BASE_DIR}/tmp/open-sans.zip
 wget https://www.1001fonts.com/download/roboto-slab.zip  -P ${BASE_DIR}/tmp
-unzip -o -d ${BASE_DIR}/src/static/vendor/fonts ${BASE_DIR}/tmp/roboto-slab.zip
+unzip -o -d ${BASE_DIR}/static/vendor/fonts ${BASE_DIR}/tmp/roboto-slab.zip
 
-python src/manage.py collectstatic -l --noinput
-
-
-
-
-
+python manage.py collectstatic -l --noinput
 
