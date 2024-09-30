7466c48b24f4b15aa0c1b66ba88240edc67877c0
Tom McAdam <mcadam1980@gmail.com>
2024-09-30 11:16:41 +0900

    Added biographies index and search

--- biographies/urls.py
+++ biographies/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 app_name = 'biographies'
 urlpatterns = [
-    path('<slug:bio_slug>/', views.show, name='show'),
+    path('', views.index, name='index'),
+    path('<slug:bio_slug>', views.show, name='show'),
 ]
--- biographies/views.py
+++ biographies/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import get_object_or_404, render
+from django.core.paginator import Paginator
 from .models import Biography
+from .pagination_helper import generate_pagination_links
 
 # Create your views here
+def index(request):
+
+    search_term = request.GET.get('search', None)
+    page_number = request.GET.get("page")
+
+    if search_term:
+        biographies = Biography.objects.filter(title__icontains=search_term)
+    else:
+        search_term = ""
+        biographies = Biography.objects.all()
+
+    paginator = Paginator(biographies, 25)
+    page_obj = paginator.get_page(page_number)
+    page_obj.pagination_links = generate_pagination_links(page_obj.number, paginator.num_pages)
+
+    return render(request, 'biographies/index.html', {"page_obj": page_obj, "search_term": search_term})
+
 def show(request, bio_slug):
     biography = get_object_or_404(Biography, slug=bio_slug)
     return render(request, 'biographies/show.html', {'biography': biography})
