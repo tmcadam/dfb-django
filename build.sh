8597bb802682b6c7963caa358dd38d3c9fc86c6e
Tom McAdam <mcadam1980@gmail.com>
2024-11-19 21:42:23 +0900

    Added fonts to 3rd party web dependencies

--- build.sh
+++ build.sh
@@ -12,10 +12,13 @@ mkdir -p ${BASE_DIR}/static/vendor/bootstrap
 cp -r ${BASE_DIR}/tmp/bootstrap-4.1.3/scss/* ${BASE_DIR}/static/vendor/bootstrap
 
 # Install fontawesome to static folder
-wget https://fontawesome.com/v4.7/assets/font-awesome-4.7.0.zip -P ${BASE_DIR}/tmp
-unzip -d ${BASE_DIR}/tmp ${BASE_DIR}/tmp/font-awesome-4.7.0.zip
-mkdir -p ${BASE_DIR}/static/vendor/fontawesome
-cp -r ${BASE_DIR}/tmp/font-awesome-4.7.0/scss/* ${BASE_DIR}/static/vendor/fontawesome
+rm -rf ${BASE_DIR}/static/vendor/fontawesome/
+wget https://use.fontawesome.com/releases/v6.6.0/fontawesome-free-6.6.0-web.zip -P ${BASE_DIR}/tmp
+unzip -d ${BASE_DIR}/tmp ${BASE_DIR}/tmp/fontawesome-free-6.6.0-web.zip
+mkdir -p ${BASE_DIR}/static/vendor/fontawesome/scss
+mkdir -p ${BASE_DIR}/static/vendor/fontawesome/webfonts
+cp -r ${BASE_DIR}/tmp/fontawesome-free-6.6.0-web/scss ${BASE_DIR}/static/vendor/fontawesome/scss
+cp -r ${BASE_DIR}/tmp/fontawesome-free-6.6.0-web/webfonts ${BASE_DIR}/static/vendor/fontawesome/webfonts
 
 
 # Install Fonts
