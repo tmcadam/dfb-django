7557ee4d0925827186f4c99415f0e23282c56b68
Tom McAdam <mcadam1980@gmail.com>
2025-08-09 23:42:18 +0900

    Update env variable name in create_admin_user script

--- scripts/create_admin_user.sh
+++ scripts/create_admin_user.sh
@@ -6,15 +6,15 @@ print (User.objects.filter(username="admin").count())
 EOF
 )
 
-if [ $admin -eq 0 ]; then 
+if [ $admin -eq 0 ]; then
 printf "Creating admin user\n"
-export DJANGO_SUPERUSER_PASSWORD=$ADMIN_PASSWORD
+export DJANGO_SUPERUSER_PASSWORD=$DJANGO_ADMIN_PASSWORD
 python manage.py createsuperuser --username admin --email admin@admin.com --no-input
 else
 printf "Updating admin password\n"
 python manage.py shell <<EOF
 from django.contrib.auth.models import User
 u = User.objects.get(username="admin")
-u.set_password("$ADMIN_PASSWORD")
+u.set_password("$DJANGO_ADMIN_PASSWORD")
 EOF
 fi
