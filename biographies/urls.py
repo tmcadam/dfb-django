44187ae1bc012a91177477365981c7eff2025632
Tom McAdam <mcadam1980@gmail.com>
2025-08-12 09:11:55 +0900

    Redirect biography urls that have ids to slug urls

--- biographies/urls.py
+++ biographies/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from . import views
+from .views import index, show_by_slug, show_by_id
 
 app_name = 'biographies'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<slug:bio_slug>', views.show, name='show'),
+    path('', index, name='index'),
+    path('<int:bio_id>', show_by_id, name='show_by_id'),
+    path('<slug:bio_slug>', show_by_slug, name='show'),
 ]
